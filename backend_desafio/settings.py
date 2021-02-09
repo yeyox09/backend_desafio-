4c4f4aa67b132ace2fb261f34acbc0269ad08fa0
Enso Vergara G <ensovergara@gmail.com>
2021-02-09 17:30:11 -0300

    fix database setup test

--- backend_desafio/settings.py
+++ backend_desafio/settings.py
@@ -97,6 +97,15 @@ elif SYSTEM_ENV == 'DEVELOPMENT':
         }
     }
 
+else:
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
